0699b09cbeaacfe58b80243d3437e1e00e256af8
adam-talbot <adam.talbot527@gmail.com>
2022-11-17 17:06:12 -0700

    started leetcode sql, solved Top Travellers

---
+++ leetcode_sql.sql
@@ -0,0 +1,9 @@
+-- 1407. Top Travellers
+-- Write an SQL query to report the distance traveled by each user.
+select name, if(sum(distance) is not null, sum(distance), 0) as travelled_distance
+from users as u
+left join rides as r on u.id = r.user_id
+group by name, user_id
+order by travelled_distance desc, name;
+
+-- still need to solve for edge case where there is a user with the same name but different id
